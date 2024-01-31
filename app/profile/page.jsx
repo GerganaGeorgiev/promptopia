5c292bc8fdfe9b7b09c37f1b39adb6764dca8553
GerganaGeorgiev <gerganag92@gmail.com>
2024-01-31 17:19:09 +0200

    Add: user can edit and delete prompts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,17 +6,34 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 function MyProfile() {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const [myPosts, setMyPosts] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+        setMyPosts(filteredPosts);
+      } catch (error) {}
+    }
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`api/users/${session.user?.id}/posts`);
       const data = await response.json();
       setMyPosts(data);
-      console.log(myPosts);
     };
     if (session?.user.id) fetchPosts();
   }, []);
@@ -25,8 +42,8 @@ function MyProfile() {
       name="My"
       desc="Welcome to your personalised profile page"
       data={myPosts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
+      handleEdit={(post) => handleEdit(post)}
+      handleDelete={(post) => handleDelete(post)}
     />
   );
 }
