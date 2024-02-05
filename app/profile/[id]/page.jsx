32ad35c68c4a09dcff16ebeb72f0982d2d24f046
GerganaGeorgiev <gerganag92@gmail.com>
2024-02-05 16:06:40 +0200

    Add: user profile preview of another users

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Profile from "@components/Profile";
+
+function UserProfile({ params }) {
+  const [posts, setPosts] = useState([]);
+
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (params?.id) fetchPosts();
+  }, [params.id]);
+
+  return (
+    <Profile
+      name={userName}
+      desc={`Welcome to ${userName} personalised profile page`}
+      data={posts}
+    />
+  );
+}
+
+export default UserProfile;
