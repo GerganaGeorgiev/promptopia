d388a78aa546827a852328c367511b919310bc44
GerganaGeorgiev <gerganag92@gmail.com>
2024-02-05 16:06:40 +0200

    Add: user profile preview of another users

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
-  const router = useRouter();
   const [copied, setCopied] = useState("");
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -17,23 +17,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image
-            src={post.creator.image}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
-          <div className="flex flex-col ">
-            <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
-            </h3>
-            <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
-            </p>
+        <Link
+          href={`/profile/${post.creator._id}?name=${post.creator.username}`}
+        >
+          <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+            <Image
+              src={post.creator.image}
+              alt="user_image"
+              width={40}
+              height={40}
+              className="rounded-full object-contain"
+            />
+            <div className="flex flex-col ">
+              <h3 className="font-satoshi font-semibold text-gray-900">
+                {post.creator.username}
+              </h3>
+              <p className="font-inter text-sm text-gray-500">
+                {post.creator.email}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
